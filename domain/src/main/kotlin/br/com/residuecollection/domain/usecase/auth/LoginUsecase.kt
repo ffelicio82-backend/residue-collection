40ce97c44d53c03faed4a62ffdf97059e9fec2d7
ffelicio82 <fernando_felicio82@yahoo.com.br>
2024-09-08 16:08:47 -0300

    :recycle: refactor: changed to adapt to the standard usecase

--- domain/src/main/kotlin/br/com/residuecollection/domain/usecase/auth/LoginUsecase.kt
+++ domain/src/main/kotlin/br/com/residuecollection/domain/usecase/auth/LoginUsecase.kt
@@ -1,14 +1,15 @@
 package br.com.residuecollection.domain.usecase.auth
 
 import br.com.residuecollection.core.Either
+import br.com.residuecollection.core.Failure
 import br.com.residuecollection.core.Usecase
 import br.com.residuecollection.domain.repository.auth.AuthRepository
-import br.com.residuecollection.domain.usecase.auth.LoginUseCase.LoginRequest
+import br.com.residuecollection.domain.usecase.auth.LoginUseCase.LoginParams
 
-class LoginUseCase(private val authRepository: AuthRepository) : Usecase<String, LoginRequest>() {
-    override suspend fun execute(request: LoginRequest) : Either<String, String> {
-        return authRepository.login(email = request.email, password = request.password)
+class LoginUseCase(private val authRepository: AuthRepository) : Usecase<String, LoginParams>() {
+    override suspend fun execute(params: LoginParams) : Either<Failure, String> {
+        return authRepository.login(email = params.email, password = params.password)
     }
 
-    data class LoginRequest(val email : String, val password : String) : Request()
+    data class LoginParams(val email : String, val password : String) : Params()
 }
