e62c073d8fa8f991985b2cd631b23d883fb6ad50
ffelicio82 <fernando_felicio82@yahoo.com.br>
2024-09-08 15:58:27 -0300

    :recycle: refactor: force left side to be Failure type

--- core/src/main/kotlin/br/com/residuecollection/core/Either.kt
+++ core/src/main/kotlin/br/com/residuecollection/core/Either.kt
@@ -1,14 +1,14 @@
 package br.com.residuecollection.core
 
-sealed class Either<out L, out R> {
-    data class Left<out L>(val value: L) : Either<L, Nothing>()
+sealed class Either<out L : Failure, out R> {
+    data class Left<out L : Failure>(val failure: L) : Either<L, Nothing>()
     data class Right<out R>(val value: R) : Either<Nothing, R>()
 }
 
-inline fun <L, R, T> Either<L, R>.fold(
+inline fun <L : Failure, R, T> Either<L, R>.fold(
     leftOperation: (L) -> T,
     rightOperation: (R) -> T
 ): T = when (this) {
-    is Either.Left -> leftOperation(value)
+    is Either.Left -> leftOperation(failure)
     is Either.Right -> rightOperation(value)
 }
